51e177c0edd2e179838a263c0ef8386f65816455
Gero0Nikolov <vtm.sunrise@gmail.com>
2016-11-05 22:15:19 +0200

    Footer.php - Global footer: Desktop version created

--- wp-content/themes/the_travel_kiwi/footer.php
+++ wp-content/themes/the_travel_kiwi/footer.php
@@ -14,11 +14,26 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
+		<div class="left-container-50p">
+			<a href="<?php echo get_site_url(); ?>" class="site-logo-link hvr-wobble-horizontal">
+				<img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo_subtitled.png" title="The Travel Kiwi" alt="The Travel Kiwi Logo Subtitled" class="site-logo" />
+			</a>
+		</div>
+		<div class="right-container-50p">
+			<a href="#" class="footer-link">About us</a>
+			<span class="splitter">&bull;</span>
+			<a href="#" class="footer-link">Facebook</a>
+			<span class="splitter">&bull;</span>
+			<a href="#" class="footer-link">Instagram</a>
+			<span class="splitter">&bull;</span>
+			<a href="#" class="footer-link">Twitter</a>
+		</div>
+		
+		<?php /*<div class="site-info">
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'the_travel_kiwi' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'the_travel_kiwi' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
 			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'the_travel_kiwi' ), 'the_travel_kiwi', '<a href="https://profiles.wordpress.org/geronikolov/" rel="designer">Gero Nikolov</a>' ); ?>
-		</div><!-- .site-info -->
+		</div> */ ?><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
