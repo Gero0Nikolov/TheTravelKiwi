31d5e0ddaf5e075ae10e12ff10b95edf3bb02113
Gero0Nikolov <vtm.sunrise@gmail.com>
2016-11-05 20:57:00 +0200

    The Travel Kiwi initial update - Including the WP installation

---
+++ wp-content/themes/the_travel_kiwi/footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package The_Travel_Kiwi
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'the_travel_kiwi' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'the_travel_kiwi' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'the_travel_kiwi' ), 'the_travel_kiwi', '<a href="https://profiles.wordpress.org/geronikolov/" rel="designer">Gero Nikolov</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
